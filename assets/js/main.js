3455d778edbced62c1d1c45e0191a1f199a1fa1f
Julius <gwolojulius2@gmail.com>
2024-09-12 15:35:22 +0300

    Contact form completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,4 +18,38 @@ navLinks.forEach((link) => {
 closeBtn.addEventListener("click", closeMenu);
 hamBtn.addEventListener("click", openMenu);
 
+/* @@@@@@@@@@ Swiper JS Library Code @@@@@@@@@@@@ */
 
+const swiper = new Swiper(".review__container", {
+  // Optional parameters
+  // direction: "horizontal",
+  loop: true,
+  spaceBetween: 30,
+  grabCursor: true,
+  slidesPreView: "auto",
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    800: {
+      slidePreView: 3,
+    },
+    1024: {
+      spaceBetween: 50,
+    },
+  },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+// Navigation arrows
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//   },
+// });
